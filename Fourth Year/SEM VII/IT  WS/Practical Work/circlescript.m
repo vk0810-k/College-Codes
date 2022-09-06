30ae84ab460b05865418f3b756873d63cc1f36b6
vk0810-k <varunkhadayate0810@gmail.com>
2022-09-06 18:13:44 +0530

    College Codes

---
+++ Fourth Year/SEM VII/IT  WS/Practical Work/circlescript.m
@@ -0,0 +1,2 @@
+% Find the Area of Circle
+A = pi*r*r % Area of Circle Formula
---
+++ Fourth Year/SEM VII/IT  WS/Practical Work/rectanglescript.m
@@ -0,0 +1,3 @@
+% Find the Area and Perimeter of rectangle
+A = l*b %Area of Rectangle
+P = 2*l*b %Perimeter of Rectangle
