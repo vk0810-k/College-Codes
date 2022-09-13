a7712dd28f89644a077b86144e6cb32c2965515c
vk0810-k <varunkhadayate0810@gmail.com>
2022-09-13 10:05:22 +0530

    College Codes

--- Fourth Year/SEM VII/IT  WS/Practical Work/Practical 8/myCubic.m
+++ Fourth Year/SEM VII/IT  WS/Practical Work/Practical 8/myCubic.m
@@ -1,6 +1,6 @@
-% Create a function M-file called myCubic.m whose output is the value of the function y=x^3+2x^2−5x−8
-% Input: x
-% Output: y
+% Description : Create a function M-file called myCubic.m whose output is the value of the function y=x^3+2x^2−5x−8
+% Input : x
+% Output : y
 % Varun Khadayate A016 
 % 13th Sep 2022
 function y = myCubic(x)
