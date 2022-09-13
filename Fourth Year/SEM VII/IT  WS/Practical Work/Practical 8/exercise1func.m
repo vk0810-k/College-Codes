6729140b9894ef9db5c449459ece69a65e59be55
vk0810-k <varunkhadayate0810@gmail.com>
2022-09-13 12:20:32 +0530

    College Codes

--- Fourth Year/SEM VII/IT  WS/Practical Work/Practical 8/exercise1func.m
+++ Fourth Year/SEM VII/IT  WS/Practical Work/Practical 8/exercise1func.m
@@ -1,5 +1,6 @@
 function p = exercise1func(x)
 p = sin(x) + x.^3;
-    % Description: find the value of function f(x) = sinx + x^3
+    % Description: find the value of function f(x) = sinx + x^3 where we
+    % find a cube, a sin value and sum of both
     % Input: 1 number: x
-    % Output: 1 number: y
+    % Output: 1 number: p
--- Fourth Year/SEM VII/IT  WS/Practical Work/Practical 8/exercise1script.m
+++ Fourth Year/SEM VII/IT  WS/Practical Work/Practical 8/exercise1script.m
@@ -1,5 +1,8 @@
-x = 3;
-y = sin(x) + x^3
-    % Description: find the value of function f(x) = sinx + x^3
-    % Input: 1 number: x
-    % Output: 1 number: y
+% x = 3;
+% y = sin(x) + x^3
+%     % Description: find the value of function f(x) = sinx + x^3 where we
+%     % find a cube, a sin value and sum of both
+%     % Input: 1 number: x
+%     % Output: 1 number: y
+
+y = rand(3, pi)
