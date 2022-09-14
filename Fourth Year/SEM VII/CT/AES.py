1309be4838444ded0c40397a3a6ff9cfc4d2d6b6
vk0810-k <varunkhadayate0810@gmail.com>
2022-09-14 23:06:29 +0530

    College Codes

--- Fourth Year/SEM VII/CT/AES.py
+++ Fourth Year/SEM VII/CT/AES.py
@@ -5,20 +5,14 @@ passwordSalt = os.urandom(16)
 key = pbkdf2.PBKDF2(password, passwordSalt).read(32)
 print('AES encryption key:', binascii.hexlify(key))
 
-# Encrypt the plaintext with the given key:
-#   ciphertext = AES-256-CTR-Encrypt(plaintext, key, iv)
-
-
-# Decrypt the ciphertext with the given key:
-#   plaintext = AES-256-CTR-Decrypt(ciphertext, key, iv)
-
-
 while True:
     ch = int(input('Welcome to AES Encryption and Decryption Program Made by Varun Khadayate..\n [*] Press 1 for Encryption \n [*] Press 2 for Decryption \n [*] Press 0 to exit..\n \nYour Choice:: '))
 
     if ch == 1:
         print("\n============================================")
         print("              !!!!Encryption!!!!              ")
+        # Encrypt the plaintext with the given key:
+        # ciphertext = AES-256-CTR-Encrypt(plaintext, key, iv)
         iv = secrets.randbits(256)
         plaintext = input("Enter the Text to be Encrypted :")
         aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
@@ -29,6 +23,8 @@ while True:
     elif ch == 2:
         print("\n============================================")
         print("               !!!Decryption!!!               ")
+        # Decrypt the ciphertext with the given key:
+        # plaintext = AES-256-CTR-Decrypt(ciphertext, key, iv)
         aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
         print("Enter the Cipher Text : ",ciphertext)
         decrypted = aes.decrypt(ciphertext)
