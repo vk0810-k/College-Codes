757adde6b9cfad2b83581dc696ce537e0ae2f926
vk0810-k <varunkhadayate0810@gmail.com>
2022-09-17 14:46:16 +0530

    College Codes

---
+++ Fourth Year/SEM VII/CT/ElGamal_Practical_7.py
@@ -0,0 +1,28 @@
+print("Key Generation Process")
+p = int(input("Enter a prime number : "))
+d = int(input("Enter a decryption key : "))
+e1 = int(input("Enter the 2nd part of Encryption Key : "))
+e2 = pow(e1,d) % p
+print("The 3rd Part of Encryption Key is : ",e2)
+print("The Public Key is : ",[e1,e2,p])
+print("\n--------------------------------------------")
+print("\nEncrption Process")
+r = int(input("Enter a random integer : "))
+c1 = pow(e1,r)%p
+print("Computer Cipher text 1 is : ",c1)
+pt = int(input("Enter the lenght of Plain Text : "))
+c2 = pt * pow(e2,r)%p
+print("Computer Cipher text 2 is : ",c2)
+print("The Cipher Text is : ",[c1,c2])
+print("\n--------------------------------------------")
+print("\nDecryption Process")
+x = pow(c1,d)
+i = 1
+while True:
+    if(i*x % p == 1):
+        D = i
+        break
+    i += 1
+
+PT = (c2*D)%p
+print("The Plain Text Length is : ",PT)
