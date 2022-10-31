03c2478132280457139b2430eecf6135014abc69
vk0810-k <varunkhadayate0810@gmail.com>
2022-10-31 20:49:45 +0530

    College Files

---
+++ Fourth Year/SEM VII/IT  WS/Class Work/Unit 7/local_max.m
@@ -0,0 +1,14 @@
+function [vals, locs]=local_max(v)
+n=length(v) ;
+vals=[];
+locs=[];
+
+for m=2:n
+    if v(m)-v(m-1)> 0 && v(m) -v(m+1)>0
+        vals=[vals ; v(m) ];
+        locs=[locs ;m];
+    end
+end
+figure; plot (v);
+hold on;
+plot (locs, vals, 'ro');
